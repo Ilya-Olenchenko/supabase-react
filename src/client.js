1d76e5507e9d24402ab8cb87e884161fbec7afc1
Ilya-Olenchenko <olenchenko.ilya@ukr.net>
2022-08-07 10:32:47 +0300

    1

---
+++ src/client.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js"
+
+export const supabase = createClient(
+    "https://idgfhkwywbtsofazjdsj.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZ2Zoa3d5d2J0c29mYXpqZHNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk4NTMwMDksImV4cCI6MTk3NTQyOTAwOX0.wkuxaqcWyb_bwgXrnPDWzZIPulSGOe2uovzeQswkeEwT"
+)
+
+// export{
+//     supabase
+// }
